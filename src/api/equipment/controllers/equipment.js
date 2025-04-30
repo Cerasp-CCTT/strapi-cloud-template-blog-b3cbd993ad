2128b448600068122ac5b161e01a0d200ec3ca2e
Sam Rivest <89708124+TwoShades@users.noreply.github.com>
2025-04-30 16:58:35 -0400

    Added Collections
    ^

--- src/api/equipment/controllers/equipment.js
+++ src/api/equipment/controllers/equipment.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * equipment controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::equipment.equipment');
